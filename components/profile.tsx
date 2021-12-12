d4e79c824c0f241799d4ea7d9d3815bdfbfa3e73
Aiandrox <aiandrox7@gmail.com>
2021-12-12 14:11:48 +0900

    コンポーネント切り分け

---
+++ components/profile.tsx
@@ -0,0 +1,40 @@
+export default function Profile() {
+  return (
+    <div className="container max-w-5xl mx-auto m-8">
+      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+        Profile
+      </h1>
+      <div className="w-full mb-4">
+        <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+      </div>
+      <div className="flex flex-wrap items-center justify-center sm:flex-row pt-6">
+        <div className="inline-block p-6">
+          <div className="rounded-full shadow">
+            <img
+              src="/images/profile.jpg"
+              height={120}
+              width={120}
+              alt="END"
+              className="rounded-full"
+            />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 p-6">
+          <div>
+            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+              END
+            </h3>
+            <p className="text-gray-600">
+              2020年からエンジニアとして働いています。
+              <br />
+              業務では、主にRailsを書いています。
+              <br />
+              <br />
+              趣味：リアル脱出ゲーム、カメラ
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import Layout, { siteTitle } from "../components/layout";
+import Profile from "../components/profile";
+import Header from "../components/header";
 import Link from "next/link";
 
 export default function Home() {
@@ -10,71 +11,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <div className="leading-normal tracking-normal text-white gradient">
-        <nav id="header" className="fixed w-full z-30 top-0 text-white">
-          <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-            <div className="pl-4 flex items-center">
-              <a
-                className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-                href="#"
-              >
-                {siteTitle}
-              </a>
-            </div>
-            <div className="block lg:hidden pr-4">
-              <button
-                id="nav-toggle"
-                className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-              >
-                <svg
-                  className="fill-current h-6 w-6"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
-              </button>
-            </div>
-            <div
-              className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
-              id="nav-content"
-            >
-              <ul className="list-reset lg:flex justify-end flex-1 items-center">
-                <li className="mr-3">
-                  <a
-                    className="inline-block py-2 px-4 text-black font-bold no-underline"
-                    href="#"
-                  >
-                    Active
-                  </a>
-                </li>
-                <li className="mr-3">
-                  <a
-                    className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                    href="#"
-                  >
-                    link
-                  </a>
-                </li>
-                <li className="mr-3">
-                  <a
-                    className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                    href="#"
-                  >
-                    link
-                  </a>
-                </li>
-              </ul>
-              <button
-                id="navAction"
-                className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-              >
-                Blog
-              </button>
-            </div>
-          </div>
-          <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-        </nav>
+        <Header></Header>
 
         <div className="pt-24">
           <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
@@ -127,127 +64,43 @@ export default function Home() {
             </g>
           </svg>
         </div>
+
         <section className="bg-white py-8">
-          <div className="container max-w-5xl mx-auto m-8">
-            <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-              About
-            </h1>
-            <div className="w-full mb-4">
-              <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-            </div>
-            <div className="flex flex-wrap items-center justify-center sm:flex-row pt-6">
-              <div className="inline-block p-6">
-                <div className="rounded-full shadow">
-                  <img
-                    src="/images/profile.jpg"
-                    height={120}
-                    width={120}
-                    alt="END"
-                    className="rounded-full"
-                  />
-                </div>
-              </div>
-              <div className="w-full sm:w-1/2 p-6">
-                <div>
-                  <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
-                    END
-                  </h3>
-                  <p className="text-gray-600">
-                    2020年からエンジニアとして働いています。
-                    <br />
-                    業務では、主にRailsを書いています。
-                    <br />
-                    <br />
-                    趣味：リアル脱出ゲーム、カメラ
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Profile></Profile>
         </section>
+
         <section className="bg-gray-100 py-8">
-          <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+          <div className="container mx-auto px-2 pt-4 pb-1 flex flex-wrap text-gray-800">
             <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-              Pricing
+              Apps
             </h1>
             <div className="w-full mb-4">
               <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
             </div>
-            <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
-              <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                  <div className="p-8 text-3xl font-bold text-center border-b-4">
-                    Free
-                  </div>
-                  <ul className="w-full text-center text-sm">
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                  </ul>
-                </div>
-                <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                  <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                    £0
-                    <span className="text-base">for one user</span>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                      Sign Up
-                    </button>
-                  </div>
-                </div>
+            {[1, 2, 3, 4].map((n) => (
+              <div key={n} className="w-full md:w-1/3 p-6 flex flex-col">
+                <Link href={`/works/200315_hikikomori`}>
+                  <a className="flex flex-wrap no-underline hover:no-underline">
+                    <div className="flex-1 overflow-hidden">
+                      <img
+                        src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+                        alt=""
+                      />
+
+                      <p className="w-full text-gray-600 text-xs md:text-sm pt-4">
+                        2021-12-1
+                      </p>
+                      <div className="w-full font-bold text-xl text-gray-800">
+                        サービス名Abcde
+                      </div>
+                    </div>
+                  </a>
+                </Link>
               </div>
-              <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
-                <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                  <div className="w-full p-8 text-3xl font-bold text-center">
-                    Basic
-                  </div>
-                  <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
-                  <ul className="w-full text-center text-base font-bold">
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                  </ul>
-                </div>
-                <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                  <div className="w-full pt-6 text-4xl font-bold text-center">
-                    £x.99
-                    <span className="text-base">/ per user</span>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                      Sign Up
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-                <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-                  <div className="p-8 text-3xl font-bold text-center border-b-4">
-                    Pro
-                  </div>
-                  <ul className="w-full text-center text-sm">
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                    <li className="border-b py-4">Thing</li>
-                  </ul>
-                </div>
-                <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                  <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                    £x.99
-                    <span className="text-base">/ per user</span>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                      Sign Up
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </section>
+
         <section className="bg-white py-8">
           <div className="container mx-auto flex flex-wrap pt-4 pb-12">
             <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
@@ -279,6 +132,7 @@ export default function Home() {
             ))}
           </div>
         </section>
+
         <svg className="wave-top" viewBox="0 0 1439 147" version="1.1">
           <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <g transform="translate(-1.000000, -14.000000)" fillRule="nonzero">
