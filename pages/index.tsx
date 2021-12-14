dfd1892a64ae07706dfdb4cf44b38255ba36104b
Aiandrox <aiandrox7@gmail.com>
2021-12-15 03:08:32 +0900

    [Add] とりあえず波にアニメーションを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,9 +27,7 @@ export default function Home() {
             <h1 className="my-4 text-5xl font-bold leading-tight">
               aiandrox.com
             </h1>
-            <p className="tracking-loose w-full">
-              Web Developer END's portfolio site.
-            </p>
+            <p className="tracking-loose w-full">Web Developer END's site.</p>
           </div>
         </div>
         <div className="relative -mt-12 lg:-mt-24">
@@ -43,15 +41,43 @@ export default function Home() {
                 <path
                   d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
                   opacity="0.100000001"
+                  ref={(ref) => {
+                    anime({
+                      targets: ref,
+                      translateY: 50,
+                      loop: true,
+                      direction: "alternate",
+                      easing: "easeInOutSine",
+                    });
+                  }}
                 ></path>
                 <path
                   d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
                   opacity="0.100000001"
+                  ref={(ref) => {
+                    anime({
+                      targets: ref,
+                      translateY: 25,
+                      loop: true,
+                      delay: 100,
+                      direction: "alternate",
+                      easing: "easeInOutSine",
+                    });
+                  }}
                 ></path>
                 <path
                   d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
                   id="Path-4"
                   opacity="0.200000003"
+                  ref={(ref) => {
+                    anime({
+                      targets: ref,
+                      translateY: 50,
+                      loop: true,
+                      direction: "alternate",
+                      easing: "easeInOutSine",
+                    });
+                  }}
                 ></path>
               </g>
               <g
@@ -59,7 +85,19 @@ export default function Home() {
                 fill="#FFFFFF"
                 fillRule="nonzero"
               >
-                <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
+                <path
+                  d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,130 L1.121,130 L0.457,34.035 Z"
+                  ref={(ref) => {
+                    anime({
+                      targets: ref,
+                      translateY: -25,
+                      loop: true,
+                      delay: 150,
+                      direction: "alternate",
+                      easing: "easeInOutSine",
+                    });
+                  }}
+                ></path>
               </g>
             </g>
           </svg>
@@ -111,23 +149,55 @@ export default function Home() {
             </div>
 
             <div className="pt-6">
-              {["株式会社サイトビジット", "株式会社資格スクエア"].map((n) => (
-                <div className="flex flex-row items-center before:content-['hello'] before:bg-blue-500 before:top-0 before:left-2 before:h-full before-w-2">
+              {[
+                [
+                  "株式会社資格スクエア",
+                  "2021-12〜現在",
+                  "システム開発グループ",
+                  "事業の分社化により株式会社資格スクエアになりました。",
+                ],
+                [
+                  "株式会社サイトビジット",
+                  "2020-9〜2021-11",
+                  "資格スクエア事業部システム開発グループ",
+                  "ご縁があってエンジニアとして働き始めました。",
+                ],
+                [
+                  "RUNTEQ",
+                  "2020-1〜2020-6",
+                  "6期生",
+                  "Railsを主に学習していました。",
+                ],
+                [
+                  "河口湖の旅館",
+                  "2019-4〜2020-1",
+                  "仲居",
+                  "寮に住み込みでいわゆるリゾートバイトをしていました。",
+                ],
+                [
+                  "小学校教師",
+                  "2018-4〜2019-3",
+                  "算数少人数",
+                  "大学卒業後、岡山県の小学校で1年間働いていました。",
+                ],
+              ].map((n) => (
+                <div
+                  data-date={n[1]}
+                  className="flex flex-row items-center before:content-[attr(data-date)] before:top-0 before:left-2 before:h-full before-w-2"
+                >
                   <div className="h-full">
                     <div className="h-10 w-10 rounded-full border"></div>
-                  </div>
-                  <div className="items-center">
-                    <p>2021-1</p>
-                    <div className="bg-indigo-600 h-2 w-px"></div>
-                    <p>2021-12</p>
                   </div>
                   <div className="shrink pl-6">
                     <div className="">
                       <h3 className="text-2xl font-bold leading-none mb-3 text-gray-800">
-                        {n}
+                        {n[0]}
                       </h3>
-                      <p className="w-full text-gray-600 text-sm md:text-sm pb-4">
-                        システム開発グループ
+                      <p className="w-full text-gray-600 text-sm md:text-sm pb-2">
+                        {n[2]}
+                      </p>
+                      <p className="w-full text-gray-600 text-md md:text-sm pb-4">
+                        {n[3]}
                       </p>
                     </div>
                   </div>
