f6d91f29c824511dbe0a2129db78a584f63e8d26
Aiandrox <aiandrox7@gmail.com>
2022-11-19 11:15:42 +0900

    [Add] Vercel Analytics を有効に

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 import { AppProps } from "next/app";
 
 const App: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
